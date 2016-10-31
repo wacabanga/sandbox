50d1a8366f586c7e8bda929e251f80b073d27350
Lawrence Wu <llwu@llwu.me>
2016-10-31 17:34:20 -0400

    conv_inversion attempt

--- conv_inversion.py
+++ conv_inversion.py
@@ -1,24 +1,61 @@
-"""Proof-of-concept inverse of tf.nn.conv2d"""
+"""Proof-of-concept inverse of 2D convolution"""
 
-import tensorflow as tf
+from typing import Tuple
 
-
-def filt_to_mat(filt, conv):
-    # should be (n x m) x (n x m)
-    pass
+import numpy as np
 
 
-def inverse_conv2d(conv, filt):
+def huge_matrix(filter_mat: np.ndarray, output_shape: Tuple[int, int]) -> np.ndarray:
     """
-    The version which takes a filter (of type tf.Constant) as input.
+    This matrix contains a huge amount of redundant information and 0's.
 
-    Assumes simple case: batch size 1, num filters 1.
+    A specialized implementation could be optimized to not explicitly create
+    this matrix.
     """
-    conv_mat = filt_to_mat(filt, conv)
-    input_dim = (filt.shape[0] + conv.shape[1] - 1,
-                 filt.shape[1] + conv.shape[2] - 1)
-    mult_op = tf.matmul(conv_mat,
-                        tf.placeholder(conv.dtype,
-                                       shape=input_dim[0] * input_dim[1]))
-    # TODO compose with flatten
-    return inverse_matmul(mult_op)  # should be from edgar's code
+    (N, M) = filter_mat.shape
+    (P, Q) = output_shape
+    dim = (N + P - 1) * (M + Q - 1)
+    huge_mat = np.zeros(dim, dim)
+    # obnoxious naive loop
+    for i in range(P):
+        for j in range(Q):
+            for k in range(N):
+                for l in range(M):
+                    huge_mat[i * Q + j][(i + j) * Q + (j + l)] = filter_mat[k][l]
+    for i in range(P * Q, dim):
+        # TODO: pick a choice which makes huge_mat invertible
+        huge_mat[i][i] = 1.0
+    return huge_mat
+
+
+def inverse_conv2d(output_mat: np.ndarray,
+                   filter_mat: np.ndarray,
+                   theta: np.ndarray) -> np.ndarray:
+    """
+    Given g and h, finds f such that f * g = h.
+
+    The dimension of theta is the number of elements in f
+    minus the number of elements in h.
+    """
+    (N, M) = filter_mat.shape
+    (P, Q) = output_mat.shape
+    dim = (N + P - 1) * (M + Q - 1)
+    assert len(theta) == dim - P * Q, 'theta dimensions wrong'
+    huge_mat = huge_matrix(filter_mat, (P, Q))
+    flattened = np.concatenate((output_mat.reshape(P * Q), theta))
+    input_flattened = np.dot(np.linalg.inv(huge_mat), flattened)
+    return input_flattened.reshape((N + P - 1, M + Q - 1))
+
+
+def test_random() -> None:
+    """Tests some random 2x2 matrices."""
+    output_mat = np.random.randn((2, 2))
+    filter_mat = np.random.randn((2, 2))
+    theta = np.random.randn(5)
+    print(output_mat)
+    print(filter_mat)
+    print(theta)
+    print(inverse_conv2d(output_mat, filter_mat, theta))
+
+if __name__ == '__main__':
+    test_random()
